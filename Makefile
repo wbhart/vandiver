8af210bacbad703037656b8b60af6036f032f487
William Hart <goodwillhart@googlemail.com>
2014-01-05 09:16:48 +0000

    Inner loop on 2, outer loop on g.

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
-	gcc -O1 vandiver.c -o vandiver -I/home/wbhart/flint2 -I/home/wbhart/mpir-git -I/home/wbhart/mpfr-3.1.1 -L/home/wbhart/flint2 -L/home/wbhart/mpir-git/.libs -L/home/wbhart/mpfr-3.1.1/src/.libs -lmpir -lmpfr -lflint
+	gcc -O2 vandiver.c -o vandiver -I/home/wbhart/flint2 -I/home/wbhart/mpir-git -I/home/wbhart/mpfr-3.1.1 -L/home/wbhart/flint2 -L/home/wbhart/mpir-git/.libs -L/home/wbhart/mpfr-3.1.1/src/.libs -lmpir -lmpfr -lflint
 
--- vandiver.c
+++ vandiver.c
@@ -46,10 +46,12 @@
 
 int main(void)
 {
-   ulong j;
+   ulong j, k, l;
    ulong m, q, qinv, q2, qinv2, w, pinv, a, g, wi;
-   ulong ap1, ap2, V1, V2, b1, c1, d1, b2, c2, d2;
-   ulong wgj, wgji, gpk1, gpk2, norm, normp, p2, pinv2;
+   ulong ap1, ap2, _ap1, _ap2, V1, V2, b1, c1, d1, b2, c2, d2;
+   ulong wgj, wgji, _wgj, _wgji, gpk1, gpk2, norm, normp, p2, pinv2;
+   ulong ord2, to2, twopk1, twopk2;
+   n_factor_t fac;
 
    ulong * Pb1 = malloc(sizeof(ulong)*1024);
    ulong * Pc1 = malloc(sizeof(ulong)*1024);
@@ -59,7 +61,7 @@ int main(void)
    ulong * Pc2 = malloc(sizeof(ulong)*1024);
    ulong * Pd2 = malloc(sizeof(ulong)*2048);
 
-   ulong p = UWORD(2147475509); 
+   ulong p = UWORD(2147475107);
    ulong k1 = UWORD(1883534374); 
    ulong k2 = UWORD(756207188); 
    
@@ -102,6 +104,8 @@ int main(void)
    
    g = n_primitive_root_prime(p);
    
+   printf("g = %ld\n", g);
+
    /* Step 5: compute wi = w^(-1) mod q */
    
    wi = n_invmod(w, q);
@@ -122,65 +126,115 @@ int main(void)
       Pd2[j] = (UWORD(1)<<norm);
    }
 
-   /* Step 7: compute gpk1 = g^(p - 1 - k1) mod p */
+   /* Step 7: find the order of 2 mod p. We know it will be a factor
+              d of p-1. Let to2 = (p - 1)/d. We then have that g^to2
+              is a power of 2 mod p. */
+
+   ord2 = p - 1;
+   n_factor_init(&fac);
+   n_factor(&fac, ord2, 1);
+
+   for (j = 0; j < fac.num; j++) /* iterate over prime factors s of p - 1 */
+   {
+      for (k = 0; k < fac.exp[j]; k++) /* iterate over powers of s */
+      {
+         if (n_powmod2_preinv(2, ord2/fac.p[j], p, pinv) == UWORD(1))
+            ord2 /= fac.p[j];
+      }
+   }
+   
+   to2 = (p - 1)/ord2;
+
+   /* We need (p - 1)/to2 to be even */
+   if (ord2 & 1) {
+      to2 /= 2;
+   } else
+      ord2 /= 2;
+
+   /* Step 8a: compute gpk1 = g^(p - 1 - k1) mod p
+           and compute twopk1 = 2^(p - 1 - k1) mod p */
    
    gpk1 = (n_powmod2_preinv(g, p - 1 - k1, p, pinv) << normp);
+   twopk1 = (n_powmod2_preinv(2, p - 1 - k1, p, pinv) << normp);
    
-   /* Step 8: compute gpk2 = g^(p - 1 - k2) mod p */
+   /* Step 8b: compute gpk2 = g^(p - 1 - k2) mod p
+           and compute twopk2 = 2^(p - 1 - k2) mod p */
 
    gpk2 = (n_powmod2_preinv(g, p - 1 - k2, p, pinv) << normp);
+   twopk2 = (n_powmod2_preinv(2, p - 1 - k2, p, pinv) << normp);
 
    /* Step 9: compute a^(p - 1 - k) mod p by cycling through a = g^j */
 
-   for (ap1 = (UWORD(1)<<normp), ap2 = (UWORD(1)<<normp), j = 1, wgj = (w<<norm), 
-                 wgji = (wi<<norm); j <= (p - 1)/2; j++) {
+   for (_ap1 = (UWORD(1)<<normp), _ap2 = (UWORD(1)<<normp), j = 1, _wgj = (w<<norm), 
+                 _wgji = (wi<<norm); j <= to2; j++) {
       ulong diff, ap1d, ap2d;
       
       /* Step 9a: set ap = (g^(p - 1 - k))^j */
 
-      ap1 = n_mulmod_preinv(ap1, gpk1, p2, pinv2, normp);
-      ap2 = n_mulmod_preinv(ap2, gpk2, p2, pinv2, normp);
+      _ap1 = n_mulmod_preinv(_ap1, gpk1, p2, pinv2, normp);
+      _ap2 = n_mulmod_preinv(_ap2, gpk2, p2, pinv2, normp);
 
       /* Step 9b: set wgj = w^(g^j) and wgji = w^(-g^j) */
 
       if (g == 2) {
-         mulmod_preinv(wgj, wgj, wgj, q2, qinv2, norm);
-         mulmod_preinv(wgji, wgji, wgji, q2, qinv2, norm);
+         mulmod_preinv(_wgj, _wgj, _wgj, q2, qinv2, norm);
+         mulmod_preinv(_wgji, _wgji, _wgji, q2, qinv2, norm);
       } else {
-         powmod_ui_preinv(wgj, wgj, g, q2, qinv2, norm);
-         powmod_ui_preinv(wgji, wgji, g, q2, qinv2, norm);
+         powmod_ui_preinv(_wgj, _wgj, g, q2, qinv2, norm);
+         powmod_ui_preinv(_wgji, _wgji, g, q2, qinv2, norm);
       }
       
-      /* Step 9c: compute b1, c1, d1 s.t. ap1 = b1 + c1*2^10 + d1*2^20 */
+      ap1  = _ap1;
+      ap2  = _ap2;
+      wgj  = _wgj;
+      wgji = _wgji;
+      
+      /* inner loop : switch to powers of 2 instead of powers of g, now
+         that we know g^j is a power of 2 mod p */
+      
+      for (k = 0; k < ord2; k++)
+      {
+         /* Step 9c: compute b1, c1, d1 s.t. ap1 = b1 + c1*2^10 + d1*2^20 */
 
-      ap1d = (ap1 >> normp);
-      ap2d = (ap2 >> normp);
+         ap1d = (ap1 >> normp);
+         ap2d = (ap2 >> normp);
 
-      b1 = (ap1d & UWORD(1023));
-      c1 = ((ap1d >> 10) & UWORD(1023));
-      d1 = (ap1d >> 20);
+         b1 = (ap1d & UWORD(1023));
+         c1 = ((ap1d >> 10) & UWORD(1023));
+         d1 = (ap1d >> 20);
 
-      /* Step 9d: compute b2, c2, d2 s.t. ap2 = b2 + c2*2^10 + d2*2^20 */
+         /* Step 9d: compute b2, c2, d2 s.t. ap2 = b2 + c2*2^10 + d2*2^20 */
 
-      b2 = (ap2d & UWORD(1023));
-      c2 = ((ap2d >> 10) & UWORD(1023));
-      d2 = (ap2d >> 20);
+         b2 = (ap2d & UWORD(1023));
+         c2 = ((ap2d >> 10) & UWORD(1023));
+         d2 = (ap2d >> 20);
 
-      /* Step 9e: compute diff = w^a - w^(-a) (up to sign but final power m is even) */
+         /* Step 9e: compute diff = w^a - w^(-a) (up to sign but final power m is even) */
+ 
+         diff = n_submod(wgj, wgji, q2);
 
-      diff = n_submod(wgj, wgji, q2);
+         /* Step 9f: accumulate product of (w^a - w^(-a)) in Pb1[b1], Pc1[c1], Pd1[d1] */
 
-      /* Step 9f: accumulate product of (w^a - w^(-a)) in Pb1[b1], Pc1[c1], Pd1[d1] */
+         Pb1[b1] = n_mulmod_preinv(Pb1[b1], diff, q2, qinv2, norm);
+         Pc1[c1] = n_mulmod_preinv(Pc1[c1], diff, q2, qinv2, norm);
+         Pd1[d1] = n_mulmod_preinv(Pd1[d1], diff, q2, qinv2, norm);
 
-      Pb1[b1] = n_mulmod_preinv(Pb1[b1], diff, q2, qinv2, norm);
-      Pc1[c1] = n_mulmod_preinv(Pc1[c1], diff, q2, qinv2, norm);
-      Pd1[d1] = n_mulmod_preinv(Pd1[d1], diff, q2, qinv2, norm);
+         /* Step 9g: accumulate product of (w^a - w^(-a)) in Pb2[b2], Pc2[c2], Pd2[d2] */
+  
+         Pb2[b2] = n_mulmod_preinv(Pb2[b2], diff, q2, qinv2, norm);
+         Pc2[c2] = n_mulmod_preinv(Pc2[c2], diff, q2, qinv2, norm);
+         Pd2[d2] = n_mulmod_preinv(Pd2[d2], diff, q2, qinv2, norm);
+      
+         /* Step 9h: set ap = (g^(p - 1 - k_))^j * (2^(p - 1 - k_))^k */
 
-      /* Step 9g: accumulate product of (w^a - w^(-a)) in Pb2[b2], Pc2[c2], Pd2[d2] */
+         ap1 = n_mulmod_preinv(ap1, twopk1, p2, pinv2, normp);
+         ap2 = n_mulmod_preinv(ap2, twopk2, p2, pinv2, normp);
 
-      Pb2[b2] = n_mulmod_preinv(Pb2[b2], diff, q2, qinv2, norm);
-      Pc2[c2] = n_mulmod_preinv(Pc2[c2], diff, q2, qinv2, norm);
-      Pd2[d2] = n_mulmod_preinv(Pd2[d2], diff, q2, qinv2, norm);
+         /* Step 9b: set wgj = (w^(g^j))^(2^k) and wgji = (w^(-g^j))^(2^k) */
+
+         mulmod_preinv(wgj, wgj, wgj, q2, qinv2, norm);
+         mulmod_preinv(wgji, wgji, wgji, q2, qinv2, norm);
+      }
    }
 
    /* Step 10: initialise V1, V2 */
